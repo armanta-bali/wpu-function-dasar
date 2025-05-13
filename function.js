2d1f3a0b027c25d6c53c94124c495507ffccbc58
Armanta Bali <armanto.rgfamily@gmail.com>
2025-05-13 11:15:21 +0800

    menambah file function.js

---
+++ function.js
@@ -0,0 +1,17 @@
+// Dasar belajar function
+
+// function declaration
+function jumblahDuaBilangan(a,b) {
+    let total;
+    total = a + b;
+
+    return total;
+}
+
+// funtion expression
+let jumlahDuaBilangan = function(a,b) {
+    let total;
+    total = a + b;
+
+    return total;
+}
