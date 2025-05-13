7cbb4124fcbc332bcd94be0ec71a69a513ab6997
Armanta Bali <armanto.rgfamily@gmail.com>
2025-05-13 13:20:30 +0800

    menyederhanakan code

--- jmlhDuaKubus.js
+++ jmlhDuaKubus.js
@@ -1,6 +1,7 @@
 // latihan 2
 // menjumlahkan dua kubus dengan menngunakan function sederhana
 
+// code biasa 
 function jumblahDuaKubus(a, b) {
     let total;
     let jumlahVolumA = a * a * a;
@@ -10,6 +11,12 @@ function jumblahDuaKubus(a, b) {
 
     return total;
 }
+
+// code refactoring lebih disederhankan
+function jumblahDuaKubus(a, b) {
+  return a ** 3 + b ** 3;
+}
+
 
 console.log(jumblahDuaKubus(8, 3));
 console.log(jumblahDuaKubus(10, 7));
